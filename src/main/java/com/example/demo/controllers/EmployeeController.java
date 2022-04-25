d8c18d9f2c41e81ec7d03821bee0f8d2d65200e0
adambl99 <adam6925@stud.kea.dk>
2022-04-25 11:24:58 +0200

    Mangler en del, employee class + html sat op. opsæt db connection samt tilføj bruger til db og finde ud fra employeeNo

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,7 +1,22 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Department;
+import com.example.demo.models.Employee;
+import com.example.demo.repositories.DepartmentRepository;
+import com.example.demo.repositories.EmployeeRepository;
+import com.example.demo.repositories.IRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
+    private final IRepository<Employee> employeeRepository = new EmployeeRepository();
+
+    @GetMapping("/employee")
+    public String allDepartments(Model model){
+       employeeRepository.getAllEntities();
+        return "employees";
+    }
 }
+
--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -1,6 +1,33 @@
 package com.example.demo.repositories;
 
-public class EmployeeRepository {
+import com.example.demo.models.Department;
+import com.example.demo.models.Employee;
+import com.example.demo.utility.DatabaseConnectionManager;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRepository implements IRepository<Employee> {
 
 
+    @Override
+    public List<Employee> getAllEntities() {
+        Connection conn = DatabaseConnectionManager.getConnection();
+        List<Employee> allEmployees = new ArrayList<Employee>();
+
+        return null;
+
+        //TODO OPSÆT DB CONNECTION
+    }
+
+    @Override
+    public Employee getSingleById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean create(Employee entity) {
+        return false;
+    }
 }
